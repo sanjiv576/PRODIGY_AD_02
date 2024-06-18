1a9a0994c7a470417c9684b2d946cbaf7f413b35
sanjiv576 <shresthasanjiv576@gmail.com>
2024-06-18 16:37:57 +0545

    add tab car accordingly

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/empty_list_widgets.dart';
+
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
@@ -7,24 +9,100 @@ class HomeView extends StatefulWidget {
   State<HomeView> createState() => _HomeViewState();
 }
 
-class _HomeViewState extends State<HomeView> {
+class _HomeViewState extends State<HomeView>
+    with SingleTickerProviderStateMixin {
+  final verticalGap = const SizedBox(height: 16);
+
+  late TabController _tabController;
+
+  final List<String> list = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            Text(
-              'Hieasf',
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            ElevatedButton(onPressed: () {}, child: const Text('data'))
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Image.asset(
+                    'assets/images/small_logo.png',
+                    width: 26,
+                    height: 26,
+                  ),
+                  Text(
+                    'TODO LIST',
+                    style: Theme.of(context).textTheme.labelLarge,
+                  ),
+                  const Spacer(),
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.search,
+                      ))
+                ],
+              ),
+              verticalGap,
+              TabBar(
+                indicatorWeight: 0.01,
+                dividerColor: Colors.transparent,
+                indicatorSize: TabBarIndicatorSize.label,
+                labelPadding: const EdgeInsets.all(12),
+                labelStyle: Theme.of(context).textTheme.labelMedium,
+                labelColor: Colors.white,
+                unselectedLabelColor: Colors.grey[600],
+                controller: _tabController,
+                tabs: const [
+                  SizedBox(
+                    width: double.infinity,
+                    child: Tab(
+                      text: 'All Lists',
+                    ),
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: Tab(
+                      text: 'Pinned',
+                    ),
+                  ),
+                ],
+              ),
+              verticalGap,
+              Expanded(
+                child: Builder(builder: (context) {
+                  if (list.isEmpty) {
+                    return const EmptyListWidgets();
+                  }
+                  return TabBarView(
+                    controller: _tabController,
+                    children: const <Widget>[
+                      Center(
+                        child: Text('all'),
+                      ),
+                      Center(
+                        child: Text('pinned'),
+                      ),
+                    ],
+                  );
+                }),
+              ),
+            ],
+          ),
         ),
       ),
     );
