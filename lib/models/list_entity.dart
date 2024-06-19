014a6c20ca0919706db51e1b785cf44c01bc9f53
sanjiv576 <shresthasanjiv576@gmail.com>
2024-06-19 13:36:11 +0545

    add todo list entities

---
+++ lib/models/list_entity.dart
@@ -0,0 +1,26 @@
+import 'todo_entity.dart';
+
+class ListEntity {
+  final int id;
+  final String title;
+  bool isPinned;
+  final String label;
+  final String date; // date when todo list is created
+  final String time; // time when todo list is created
+  final List<TodoEntity> todos;
+
+  ListEntity({
+    required this.id,
+    required this.title,
+    bool? isPinned,
+    required this.label,
+    required this.date,
+    required this.todos,
+    required this.time,
+  }) : isPinned = isPinned ?? false;
+
+  @override
+  String toString() {
+    return 'id: $id, title: $title, isPinned: $isPinned, label: $label, todo: $todos, date: $date, time: $time';
+  }
+}
---
+++ lib/models/todo_entity.dart
@@ -0,0 +1,17 @@
+class TodoEntity {
+  final int id;
+  final String todo;
+
+  final bool isComplete;
+
+  TodoEntity({
+    required this.id,
+    required this.todo,
+    bool? isComplete,
+  }) : isComplete = isComplete ?? false;
+
+  @override
+  String toString() {
+    return 'todo id: $id, todo: $todo, isComplete: $isComplete';
+  }
+}
