e1ac1b67199f1e3e39a965bf96ef29057f0e8cd4
sanjiv576 <shresthasanjiv576@gmail.com>
2024-06-19 20:21:15 +0545

    make static

--- lib/services/date_time_converter.dart
+++ lib/services/date_time_converter.dart
@@ -1,11 +1,11 @@
 import 'package:intl/intl.dart';
 
 class DateTimeConverter {
-  String getCurrentTime() {
+ static String getCurrentTime() {
     return DateFormat.jm().format(DateTime.now()); // for example: 10:20 AM
   }
 
-  String getCurrentDate() {
+ static String getCurrentDate() {
     return DateFormat.yMMMd()
         .format(DateTime.now()); //format example: Jun 19, 2024
   }
