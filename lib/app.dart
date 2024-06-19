e0f31f2374ae2eb2f3284b5f952ae6d615280e5d
sanjiv576 <shresthasanjiv576@gmail.com>
2024-06-19 08:47:13 +0545

    implement app theme changing

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'router/app_routes.dart';
 import 'themes/app_theme.dart';
+import 'view/home_view.dart';
 
-class AppView extends StatelessWidget {
+// class AppView extends StatelessWidget {
+//   const AppView({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       theme: AppThemes.appDarkTheme(),
+//       initialRoute: AppRoutes.introRoute,
+//       routes: AppRoutes.getAppRoutes(),
+//     );
+//   }
+// }
+
+class AppView extends ConsumerWidget {
   const AppView({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: AppThemes.appDarkTheme(),
+      theme: ref.watch(isDarkThemeProvider)
+          ? AppThemes.appDarkTheme()
+          : AppThemes.appLightTheme(),
       initialRoute: AppRoutes.introRoute,
       routes: AppRoutes.getAppRoutes(),
     );
