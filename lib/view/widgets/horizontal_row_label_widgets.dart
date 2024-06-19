653325db5d99d30053d50f2af065ff8e9c7a8073
sanjiv576 <shresthasanjiv576@gmail.com>
2024-06-19 13:37:36 +0545

    add bottom label in Card

---
+++ lib/view/widgets/horizontal_row_label_widgets.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import '../../models/list_entity.dart';
+
+class HorizontalRowLabelWidgets extends StatelessWidget {
+  const HorizontalRowLabelWidgets({
+    super.key,
+    required this.singleList,
+  });
+
+  final ListEntity singleList;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Card(
+          elevation: 5,
+          color: Colors.black,
+          child: Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: Text(
+              singleList.label,
+              style: Theme.of(context)
+                  .textTheme
+                  .bodySmall!
+                  .copyWith(color: Colors.white),
+            ),
+          ),
+        ),
+        const SizedBox(width: 8),
+        Text(
+          singleList.date,
+          style: Theme.of(context).textTheme.bodySmall,
+        ),
+        const SizedBox(width: 8),
+        Text(
+          singleList.time,
+          style: Theme.of(context).textTheme.bodySmall,
+        ),
+      ],
+    );
+  }
+}
