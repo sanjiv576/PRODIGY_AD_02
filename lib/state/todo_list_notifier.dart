49f6303685ac07368e84bdc0810926d0e3ff69fe
sanjiv576 <shresthasanjiv576@gmail.com>
2024-06-20 12:03:11 +0545

    manage state of the listt

---
+++ lib/state/todo_list_notifier.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../models/list_entity.dart';
+
+class TodoListNotifier extends StateNotifier<List<ListEntity>> {
+  TodoListNotifier() : super([]);
+
+  void setTodoList(List<ListEntity> todoList) {
+    state = todoList;
+  }
+
+  void addTodoList(ListEntity newList) {
+    state = [...state, newList];
+  }
+
+  void updateTodoList(ListEntity updatedList) {
+    state = [
+      for (final list in state)
+        if (list.id == updatedList.id) updatedList else list
+    ];
+  }
+
+  void removeTodoList(String id) {
+    state = state.where((list) => list.id != id).toList();
+  }
+}
+
+final todoListProvider = StateNotifierProvider<TodoListNotifier, List<ListEntity>>((ref) {
+  return TodoListNotifier();
+});
